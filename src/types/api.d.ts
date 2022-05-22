10c0c7690bc4f28a238e239b648fafc5f56ac83a
Mathis Le Bonniec <mathis.le-bonniec@epitech.eu>
2022-05-22 23:52:33 +0200

    feat(user): add user interface

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,6 +1,15 @@
-export interface ITodos {
+export interface ITodo {
   id: number;
   title: string;
   description: string;
   status: string;
+}
+
+export interface IUser {
+  id: number;
+  email: string;
+  password: string;
+  name: string;
+  firstname: string;
+  created_at: Date;
 }
